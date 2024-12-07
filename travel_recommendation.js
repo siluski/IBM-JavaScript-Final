e93a26bcca576036b60533eaf293d476c1cfab53
siluski <arsiluk@optonline.net>
2024-12-07 13:27:57 -0500

    added search logic

---
+++ travel_recommendation.js
@@ -0,0 +1,55 @@
+const locations = [];
+
+const searchBtn = document.getElementById('searchBtn');
+//console.log(searchBtn);
+searchBtn.addEventListener("click", getLocations);
+
+function getLocations(){
+    //console.log("Called Function");
+    const input = document.getElementById("search").value.toLowerCase();
+
+    fetch('travel_recommendation_api.json').then(response => response.json()).then(data =>{
+        let location;
+        if(validateBeach(input)){
+            location = data.beaches;
+        }
+        else if(validateTemples(input)){
+            location = data.temples;
+        }else{
+            location = validateCountry(input,data);
+            if(location === null){
+                alert("No data could be found, please enter a valid search term (beaches, temples, countries)");
+            }
+        }
+
+        // const location = data.countries.find(item=>item.name === 'Brazil');
+        console.log(location);
+    
+    }).catch(error=>{
+        //console.log("ERROR");
+        console.error(error);
+        alert("Error while fetching your data, please try again with different input");
+    })
+}
+
+function validateBeach(input){
+    if(input !== 'beach' && input !== 'beaches'){
+        return false;
+    }
+    return true;
+}
+
+function validateTemples(input){
+    if(input !== 'temple' && input !=='temples'){
+        return false;
+    }
+    return true;
+}
+
+function validateCountry(input, data){
+    let countryInfo = data.countries.find(country=>country.name.toLowerCase()===input);
+    if(!countryInfo){
+        return null;
+    }
+    return countryInfo;
+}
