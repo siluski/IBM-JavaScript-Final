1c47570b3decdac3a2946141ab6da27cbf8d284a
siluski <47801828+siluski@users.noreply.github.com>
2024-12-16 22:34:02 -0500

    Update travel_recommendation.js

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,24 +1,57 @@
-const locations = [];
+let locations = [];
 const searchBtn = document.getElementById('searchBtn');
+const clearBtn = document.getElementById('resetSearchBtn');
 searchBtn.addEventListener("click", getLocations);
+clearBtn.addEventListener("click", clearSearch);
+
 
 function getLocations(){
     const input = document.getElementById("search").value.toLowerCase();
-
+    let resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
     fetch('travel_recommendation_api.json').then(response => response.json()).then(data =>{
-        let location;
+        //let location;
         if(validateBeach(input)){
-            location = data.beaches;
+            locations = data.beaches;
+            console.log("HERE");
+            locations.forEach(location =>{
+                resultDiv.innerHTML+='<div id = "location">';
+                resultDiv.innerHTML+=`<img src=${location.imageUrl}>`;
+                resultDiv.innerHTML+=`<h3>${location.name}</h3>`
+                resultDiv.innerHTML+=`<p>${location.description}</p>`
+                resultDiv.innerHTML+='</div>'
+            });
         }
         else if(validateTemples(input)){
-            location = data.temples;
+            locations = data.temples;
+            locations.forEach(location =>{
+                resultDiv.innerHTML+='<div id = "location">';
+                resultDiv.innerHTML+=`<img src=${location.imageUrl}>`;
+                resultDiv.innerHTML+=`<h3>${location.name}</h3>`
+                resultDiv.innerHTML+=`<p>${location.description}</p>`
+                resultDiv.innerHTML+='</div>'
+            });
         }else{
-            location = validateCountry(input,data);
-            if(location === null){
+            locations = validateCountry(input,data);
+            if(locations === null){
                 alert("No data could be found, please enter a valid search term (beaches, temples, countries)");
+            }else{
+                //locations = data.countries;
+                locations.forEach(location=>{
+                    resultDiv.innerHTML+='<div id="location">';
+                    resultDiv.innerHTML+=`<h3>${location.name}</h3>`;
+                    location.cities.forEach(city=>{
+                        resultDiv.innerHTML+='<div id="city">';
+                        resultDiv.innerHTML+=`<img src=${city.imageUrl}`;
+                        resultDiv.innerHTML+=`<h5>${city.name}<h5>`;
+                        resultDiv.innerHTML+=`<p>${city.description}<p>`;
+                        resultDiv.innerHTML+='</div>';
+                    });
+                    resultDiv.innerHTML+='</div>';
+                });
             }
         }
-        console.log(location);
+        console.log(locations);
     }).catch(error=>{
         console.error(error);
         alert("Error while fetching your data, please try again with different input");
@@ -46,3 +79,12 @@ function validateCountry(input, data){
     }
     return countryInfo;
 }
+
+function clearSearch(){
+    let resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+    locations = [];
+    document.getElementById("search").value = '';
+
+}
+
